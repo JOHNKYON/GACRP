ce57cccf3b6e0534419385e3068efbc016f5d113
JOHNKYON <zhao_fengbo@outlook.com>
2018-10-01 15:39:37 +0900

    Itegrating with travis-ci.

---
+++ test/test_fake.py
@@ -0,0 +1,15 @@
+"""
+This is a fake test to verify if test runs correctly and pass Travis-CI test when there's
+no real test case.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+class TestFake:
+
+    def test_fake(self):
+        assert 0 == 0
+
